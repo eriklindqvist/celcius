283664a3c6b9228454347d3eb6880ea06fc80cf9
Erik Lindqvist <erik.lindqvist@readly.com>
2015-09-29 15:41:53 +0200

    Check threshold when setting new value

--- models/value.rb
+++ models/value.rb
@@ -6,7 +6,7 @@ class Value
     until !!(metric.values[t.hour.to_s]||{})[t.min.to_s] || (t.hour == 0 && t.min == 0); t -= 60; end
     current_value = (metric.values[t.hour.to_s]||{})[t.min.to_s]
 
-    unless value == current_value
+    unless (value - current_value).abs < 0.1
       metric.set_value(time, value)
       metric.save
     end
