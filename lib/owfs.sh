12728933298938f315d0c01385741da9e88cddd4
eriklindqvist <erikjo82+github@gmail.com>
2016-03-14 11:28:17 +0100

    Improved owfs bash script

--- lib/owfs.sh
+++ lib/owfs.sh
@@ -7,19 +7,24 @@ url=http://localhost:9393
 
 #cd $owfs
 
-for sensor in `ssh root@dockstar2 'cd /mnt/owfs && ls -1d 10.*'`
-do
-  temperature=`ssh root@dockstar2 cat /mnt/owfs/$sensor/temperature | sed 's/^ *//g'`
-  curl -X POST -d "sensor=$sensor&value=$temperature" $url/temperature
-done
-
 while true
 do
-  for sensor in `ssh root@dockstar2 'cd /mnt/owfs && ls -1d 1D.*'`
+  for sensor in `ssh root@dockstar2 'cd /mnt/owfs && ls -1d 10.*'`
   do
-    pulses=`ssh root@dockstar2 cat /mnt/owfs/$sensor/counters.B`
-    curl -X POST -d "sensor=$sensor&value=$pulses" $url/pulses
+    temperature=`ssh root@dockstar2 cat /mnt/owfs/$sensor/temperature | sed 's/^ *//g'`
+    curl -X POST -d "sensor=$sensor&value=$temperature" $url/temperature
   done
-  sleep 15
+
+  i=0
+  while [ $i -lt 4 ]
+  do
+    for sensor in `ssh root@dockstar2 'cd /mnt/owfs && ls -1d 1D.*'`
+    do
+      pulses=`ssh root@dockstar2 cat /mnt/owfs/$sensor/counters.B`
+      curl -X POST -d "sensor=$sensor&value=$pulses" $url/pulses
+    done
+    i=$(( $i + 1 ))
+    sleep 15
+  done
 done
 #cd $pwd
