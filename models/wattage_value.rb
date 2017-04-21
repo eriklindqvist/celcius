dac05c9bbe59f1bee5e02bae3949672c90cf61c6
eriklindqvist <erikjo82+github@gmail.com>
2017-04-21 13:34:53 +0200

    Simulate pulses for sensors reporting in watts

--- models/wattage_value.rb
+++ models/wattage_value.rb
@@ -14,7 +14,7 @@ class WattageValue < Value
 
     current_value = (metric.values[t.hour.to_s]||{})[t.min.to_s]
 
-    metric.set_value(time, watts) if watts    
+    metric.set_value(time, watts) if watts
 
     metric.pulses = pulses
     metric.save
@@ -27,12 +27,34 @@ class WattageValue < Value
     t = time.dup
     until !!(metric.values[t.hour.to_s]||{})[t.min.to_s] || (t.hour == 0 && t.min == 0); t -= 60; end
 
-    current_value = (metric.values[t.hour.to_s]||{})[t.min.to_s]
+    existing_value = (metric.values[t.hour.to_s]||{})[t.min.to_s]
+    values = metric.values.map{|h,vals| vals.map{|m,values| [DateTime.new(time.year, time.month, time.day, h.to_i, m.to_i, 0, time.zone), values] }}.flatten(1).to_h
 
-    if value && (current_value.nil? || (value - current_value).abs > 0)
-      metric.set_value(time, value)
+    unless existing_value
+      metric.set_value(time, value) if value
+
+      last_time, last_value = values.to_a.last
+
+      # Only allow update of fake pulses value if inserting at the end of the values array
+      if last_time < time
+        rate = sensor.rate || 10000
+
+        hours = hours(last_time, time)
+        kwh = (last_value * hours + ((last_value - value).abs * hours)/2)/1000
+
+        pulses = kwh * rate
+
+        metric.pulses += pulses
+      end
+
+      metric.save
     end
+  end
 
-    metric.save
+  private
+  # Level 2 notation just for fun.
+  # Return difference in hours between two DateTime objects
+  def hours(*times)
+    times[0..1].map(&:to_time).reduce(&:-).abs / 3600
   end
 end
